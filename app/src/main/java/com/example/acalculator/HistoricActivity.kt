f3a031762195d4fd897503d585fe17b325b0c75c
Francisco Madruga <madruga.francisco@gmail.com>
2020-04-07 16:32:41 +0100

    Drawer

--- app/src/main/java/com/example/acalculator/HistoricActivity.kt
+++ app/src/main/java/com/example/acalculator/HistoricActivity.kt
@@ -3,16 +3,50 @@ package com.example.acalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_historic.*
 
 
-class HistoricActivity : AppCompatActivity() {
+class HistoricActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_historic)
+        setSupportActionBar(toolbar)
         NavigationManager.goToHistoricFragment(supportFragmentManager)
+        setupDrawerMenu()
+    }
+
+    private fun setupDrawerMenu() {
+        val toogle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close)
+        nav_drawer.setNavigationItemSelectedListener(this)
+        drawer.addDrawerListener(toogle)
+        toogle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_calculator -> NavigationManager.goToCalculatorFragment(supportFragmentManager)
+            R.id.nav_historic -> NavigationManager.goToHistoricFragment(supportFragmentManager)
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        }
+        else if (supportFragmentManager.backStackEntryCount == 1) {
+            finish()
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/acalculator/MainActivity.kt
+++ app/src/main/java/com/example/acalculator/MainActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_calculator.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val TAG = MainActivity::class.java.simpleName
 
@@ -19,8 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.i(TAG, "O metodo onCreate foi invocado")
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         NavigationManager.goToCalculatorFragment(supportFragmentManager)
-
+        setupDrawerMenu()
         val historic = intent?.getParcelableArrayListExtra<Operation>(EXTRA_HISTORIC)?.toMutableList()?:mutableListOf()
         list_historic?.layoutManager = LinearLayoutManager(this)
         list_historic?.adapter = HistoricAdapter(this, R.layout.item_expression, historic)
@@ -31,6 +35,35 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        }
+        else if (supportFragmentManager.backStackEntryCount == 1) {
+            finish()
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun setupDrawerMenu() {
+        val toogle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.drawer_open, R.string.drawer_close)
+        nav_drawer.setNavigationItemSelectedListener(this)
+        drawer.addDrawerListener(toogle)
+        toogle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_calculator -> {
+                NavigationManager.goToCalculatorFragment(supportFragmentManager)
+            }
+            R.id.nav_historic -> NavigationManager.goToHistoricFragment(supportFragmentManager)
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         text_visor.text = savedInstanceState?.getString(VISOR_KEY)
@@ -38,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         outState.run {
-            putString(VISOR_KEY, text_visor.text.toString())
+            putString(VISOR_KEY, text_visor?.text.toString())
         }
         super.onSaveInstanceState(outState)
     }
