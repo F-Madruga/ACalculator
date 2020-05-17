ed244454c9166d4ead168351af9cf8b2ea224e81
Francisco Madruga <madruga.francisco@gmail.com>
2020-05-17 19:42:44 +0100

    Entrega

--- app/src/main/java/pt/ulusofona/cm/ui/activities/LoginActivity.kt
+++ app/src/main/java/pt/ulusofona/cm/ui/activities/LoginActivity.kt
@@ -1,10 +1,13 @@
 package pt.ulusofona.cm.ui.activities
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import pt.ulusofona.cm.R
+import pt.ulusofona.cm.ui.fragments.PREFERENCES_FILE
 import pt.ulusofona.cm.ui.utils.NavigationManager
 
 class LoginActivity : AppCompatActivity() {
@@ -16,7 +19,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setSupportActionBar(toolbar)
-        NavigationManager.goToLoginFragment(supportFragmentManager)
+        if (getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE) != null && getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE).getString("token", "") != "") {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else {
+            NavigationManager.goToLoginFragment(supportFragmentManager)
+        }
     }
 
     override fun onStart() {
